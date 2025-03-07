3e8508a505467ebdee33c844d6cd7b729832d92f
Ritastrophic <novichkovarita@icloud.com>
2025-03-07 16:43:44 +0300

    Добавлена вторая лабораторная работа

--- FirstLab2/app/build.gradle.kts
+++ FirstLab2/app/build.gradle.kts
@@ -59,4 +59,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("commons-codec:commons-codec:1.15")
 }
--- FirstLab2/app/src/main/java/ru/iu3/firstlab2/MainActivity.java
+++ FirstLab2/app/src/main/java/ru/iu3/firstlab2/MainActivity.java
@@ -1,25 +1,32 @@
 package ru.iu3.firstlab2;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 import ru.iu3.firstlab2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'firstlab2' library on application startup.
     static {
         System.loadLibrary("firstlab2");
         System.loadLibrary("mbedcrypto");
     }
 
     private ActivityMainBinding binding;
+    private ActivityResultLauncher<Intent> activityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,28 +35,58 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
-
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            String pin = data.getStringExtra("pin");
+                            Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
         int randomRes = initRng();
-        byte[] byteArr = randomBytes(20);
-        byte[] KEY = randomBytes(16);
-        Log.d("firstlab2_ndk", Arrays.toString(byteArr));
-        byteArr = encrypt(KEY, byteArr);
-        Log.d("firstlab2_ndk", Arrays.toString(byteArr));
-        byteArr = decrypt(KEY, byteArr);
-        Log.d("firstlab2_ndk", Arrays.toString(byteArr));
-        byte[] encryptedTextBytes = encrypt(KEY, tv.getText().toString().getBytes(StandardCharsets.UTF_8));
-        String decryptedText = new String(decrypt(KEY, encryptedTextBytes), StandardCharsets.UTF_8);
-        Log.d("firstlab2_ndk", Arrays.toString(tv.getText().toString().getBytes(StandardCharsets.UTF_8)));
-        Log.d("firstlab2_ndk", Arrays.toString(decrypt(KEY, encryptedTextBytes)));
+        // Example of a call to a native method
+//        TextView tv = binding.sampleText;
+//        tv.setText(stringFromJNI());
+//
+//        byte[] byteArr = randomBytes(20);
+//        byte[] KEY = randomBytes(16);
+//        Log.d("firstlab2_ndk", Arrays.toString(byteArr));
+//        byteArr = encrypt(KEY, byteArr);
+//        Log.d("firstlab2_ndk", Arrays.toString(byteArr));
+//        byteArr = decrypt(KEY, byteArr);
+//        Log.d("firstlab2_ndk", Arrays.toString(byteArr));
+//        byte[] encryptedTextBytes = encrypt(KEY, tv.getText().toString().getBytes(StandardCharsets.UTF_8));
+//        String decryptedText = new String(decrypt(KEY, encryptedTextBytes), StandardCharsets.UTF_8);
+//        Log.d("firstlab2_ndk", Arrays.toString(tv.getText().toString().getBytes(StandardCharsets.UTF_8)));
+//        Log.d("firstlab2_ndk", Arrays.toString(decrypt(KEY, encryptedTextBytes)));
+//
+
     }
 
-    /**
-     * A native method that is implemented by the 'firstlab2' native library,
-     * which is packaged with this application.
-     */
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch(DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
+    public void onButtonClick(View v)
+    {
+        int randomRes = initRng();
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        activityResultLauncher.launch(it);
+    }
     public native String stringFromJNI();
     public static native int initRng();
     public static native byte[] randomBytes(int no);
