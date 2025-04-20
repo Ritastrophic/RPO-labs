3c40176f7025e3295bb9b49484052673b845e441
Ritastrophic <novichkovarita@icloud.com>
2025-04-21 01:00:33 +0300

    Добавлена 7 лабораторная работа

---
+++ lab7_backend/src/test/java/ru/iu3/lab7_backend/Lab7BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iu3.lab7_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
