6827323cca47a4c956ea038d285d4b7ba7d6b0a2
Ritastrophic <novichkovarita@icloud.com>
2025-05-18 21:38:50 +0300

    Добавлена 8 лабораторная работа

---
+++ lab8_backend/src/main/java/ru/iu3/lab7_backend/controllers/ArtistController.java
@@ -0,0 +1,91 @@
+package ru.iu3.lab7_backend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import ru.iu3.lab7_backend.models.Artist;
+import ru.iu3.lab7_backend.models.Country;
+import ru.iu3.lab7_backend.repositories.ArtistRepository;
+import ru.iu3.lab7_backend.repositories.CountryRepository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ArtistController {
+    @Autowired
+    ArtistRepository artistRepository;
+
+    @Autowired
+    CountryRepository countryRepository;
+
+    @GetMapping("/artists")
+    public List
+    getAllArtists() {
+        return artistRepository.findAll();
+    }
+
+    @PostMapping("/artists")
+    public ResponseEntity<Object> createArtist(@RequestBody Artist artist)
+            throws Exception {
+        if (artist.getName() == null || artist.getName().trim().isEmpty()) {
+            Map<String, String> errorResponse = new HashMap<>();
+            errorResponse.put("error", "name is null");
+            return ResponseEntity.badRequest().body(errorResponse); //  HttpStatus.BAD_REQUEST;
+        }
+        try {
+            Optional<Country>
+                    cc = countryRepository.findById(artist.country.id);
+            if (cc.isPresent()) {
+                artist.country = cc.get();
+            }
+            Artist nc = artistRepository.save(artist);
+            return new ResponseEntity<Object>(nc, HttpStatus.OK);
+        } catch (Exception ex) {
+            String error;
+            if (ex.getMessage().contains("artists.name_UNIQUE"))
+                error = "artist already exists";
+            else
+                error = "undefined error";
+            Map<String, String>
+                    map = new HashMap<>();
+            map.put("error", error);
+            return new ResponseEntity<Object>(map, HttpStatus.OK);
+        }
+    }
+
+    @PutMapping("/artists/{id}")
+    public ResponseEntity<Artist> updateArtist(@PathVariable(value = "id") Long artistId,
+                                               @RequestBody Artist artistDetails) {
+        Artist artist = null;
+        Optional<Artist>
+                cc = artistRepository.findById(artistId);
+        if (cc.isPresent()) {
+            artist = cc.get();
+            artist.name = artistDetails.name;
+            artistRepository.save(artist);
+            return ResponseEntity.ok(artist);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "artist not found");
+        }
+    }
+
+    @DeleteMapping("/artists/{id}")
+    public ResponseEntity<Object> deleteArtist(@PathVariable(value = "id") Long artistId) {
+        Optional<Artist>
+                artist = artistRepository.findById(artistId);
+        Map<String, Boolean>
+                resp = new HashMap<>();
+        if (artist.isPresent()) {
+            artistRepository.delete(artist.get());
+            resp.put("deleted", Boolean.TRUE);
+        } else
+            resp.put("deleted", Boolean.FALSE);
+        return ResponseEntity.ok(resp);
+    }
+}
---
+++ lab8_backend/src/main/java/ru/iu3/lab7_backend/controllers/MuseumController.java
@@ -0,0 +1,79 @@
+package ru.iu3.lab7_backend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import ru.iu3.lab7_backend.models.Museum;
+import ru.iu3.lab7_backend.repositories.MuseumRepository;
+import ru.iu3.lab7_backend.repositories.UserRepository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("api/v1/museums")
+public class MuseumController {
+
+    @Autowired
+    MuseumRepository museumRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping()
+    public ResponseEntity<List<Museum>> getAllMuseums() {
+        return ResponseEntity.ok(museumRepository.findAll());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Object> createMuseum(@RequestBody Museum museum) {
+        try {
+            Museum nm = museumRepository.save(museum);
+            return ResponseEntity.ok(nm);
+        } catch(Exception ex) {
+            String error;
+            if (ex.getMessage().contains("museums_name_key")) {
+                error = "museumalreadyexists";
+            } else {
+                error = "undefinederror";
+            }
+            Map<String, String> map = new HashMap<>();
+            map.put("error", error);
+            return ResponseEntity.ok(map);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Museum> updateMuseum(@PathVariable(value = "id") Long museumId,
+                                                 @RequestBody Museum museumDetails) {
+        Museum museum = null;
+        Optional<Museum>
+                cc = museumRepository.findById(museumId);
+        if (cc.isPresent()) {
+            museum = cc.get();
+            museum.name = museumDetails.name;
+            museumRepository.save(museum);
+            return ResponseEntity.ok(museum);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "museum not found");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteMuseum(@PathVariable(value = "id") Long museumId) {
+        Optional<Museum> museum = museumRepository.findById(museumId);
+        Map<String, Boolean> resp = new HashMap<>();
+        if (museum.isPresent()) {
+            museumRepository.delete(museum.get());
+            resp.put("deleted", Boolean.TRUE);
+        } else {
+            resp.put("deleted", Boolean.FALSE);
+        }
+        return ResponseEntity.ok(resp);
+    }
+}
